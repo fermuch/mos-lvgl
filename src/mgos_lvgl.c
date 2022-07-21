36cbd10e3e0e1190c94158a6095cfe801d15be7e
Fernando Mumbach <fermuch@gmail.com>
2022-07-21 16:39:23 -0300

    minor fix: returning correct value for correct initialization

--- src/mgos_lvgl.c
+++ src/mgos_lvgl.c
@@ -36,5 +36,5 @@ bool mgos_mos_lvgl_init(void) {
   mgos_set_timer(mgos_sys_config_get_lvgl_time_ms(), MGOS_TIMER_REPEAT, lvgl_inc_timer_cb, NULL);
   mgos_set_timer(mgos_sys_config_get_lvgl_time_ms(), MGOS_TIMER_REPEAT, lvgl_task_handler_cb, NULL);
 
-  return MGOS_APP_INIT_SUCCESS;
+  return true;
 }
